75e2a2db2630c92666b51b775d06bb8d6b1e38c2
slimek <slime573@gmail.com>
2015-02-11 14:13:01 +0800

    [NDK] Fix Application.mk : conditional compiling.
    - In ndk-build, we can only depends on NDK_DEBUG define. Remove NDEBUG=1 in Release configuration.
    - (refactoring) Rearrange Application.mk file layout.

--- build/proj.android/jni/Application.mk
+++ build/proj.android/jni/Application.mk
@@ -1,24 +1,31 @@
+NDK_TOOLCHAIN_VERSION := 4.8
+
 APP_PLATFORM := android-15
-APP_ABI := x86 armeabi armeabi-v7a
 APP_STL := gnustl_static
 
 APP_CFLAGS := -fexceptions
 APP_CPPFLAGS := -std=c++11 -frtti
 
-ifeq ($(NDEBUG),1)
+APP_ABI := x86 armeabi armeabi-v7a
 
-# Release
-APP_OPTIM := release
-COCOS_CONFIG := gcc48.Release
 
-else
+ifeq ($(NDK_DEBUG),1)
 
 # Debug
 APP_OPTIM := debug
+
 COCOS_CONFIG := gcc48.Debug
+
 APP_CFLAGS += -g -DCOCOS2D_DEBUG=1
+
+else
+
+# Release
+APP_OPTIM := release
+
+COCOS_CONFIG := gcc48.Release
 
 endif
 
+
 NDK_APP_OUT := obj/$(COCOS_CONFIG)
-NDK_TOOLCHAIN_VERSION := 4.8
